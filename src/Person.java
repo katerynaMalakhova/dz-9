96d3742b57d5d1e3641c159924344499a9dea03a
katerynaMalakhova <kateryna.malakhov@gmail.com>
2023-05-04 21:28:05 +0200

    dz-9

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,65 @@
 public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Person partner;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person getPartner() {
+        return partner;
+    }
+
+    public void setPartner(Person partner) {
+        this.partner = partner;
+    }
+
+    public void isRetired(Person person){};
+
+    public void registerPartnership(Person partner){
+    };
+
+    public void deregisterPartnership(){
+    };
+
+    public void print(){
+        System.out.printf("Name: %s\n", this.getFirstName());
+        System.out.printf("Last name: %s\n", this.getLastName());
+        System.out.printf("Age: %d\n", this.getAge());
+        if (this.getPartner() == null)
+        System.out.println("Not in a relationship\n");
+        else System.out.printf("Married to: %s %s\n",this.getPartner().getLastName(), this.getPartner().getFirstName());
+    }
+
 }
