dfbba185a3d40f91e4738b71b260b90279e0591e
katerynaMalakhova <kateryna.malakhov@gmail.com>
2023-05-05 09:57:28 +0200

    fix comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person man = new Man("manName", "manLastName", 60);
+        Person woman = new Woman("womanName", "womanLastName", 60);
+
+        man.print();
+        woman.print();
+
+        man.isRetired(man);
+        woman.isRetired(woman);
+
+        man.registerPartnership(woman);
+        woman.registerPartnership(man);
+        System.out.println("Partnership registered");
+        man.print();
+        woman.print();
+
+        man.deregisterPartnership();
+        woman.deregisterPartnership();
+        System.out.println("Partnership deregistered");
+        man.print();
+        woman.print();
     }
+
 }
--- src/Woman.java
+++ src/Woman.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Woman {
+public class Woman extends Person{
+
+    private String sex;
+private String maidenName;
+    public Woman(String firstName, String lastName, int age) {
+        super(firstName, lastName, age);
+        this.maidenName = lastName;
+        this.sex = "female";
+    }
+
+    public String getMaidenName() {
+        return maidenName;
+    }
+
+    public void setMaidenName(String maidenName) {
+        this.maidenName = maidenName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void isRetired(Person person){
+        if (person.getAge() >= 60)
+            System.out.printf("The person %s %s is retired\n", person.getFirstName(), person.getLastName());
+        else  System.out.printf("The person %s %s is not retired\n", person.getFirstName(), person.getLastName());
+    };
+
+    public void registerPartnership(Person man){
+        this.setPartner(man);
+        this.setLastName(man.getLastName());
+    };
+
+    public void deregisterPartnership(){
+        this.setPartner(null);
+        this.setLastName(this.getMaidenName());
+    };
+    public void changeLastNameAfterDivorce(Woman woman){
+        woman.setLastName(woman.getMaidenName());
+    };
 }
